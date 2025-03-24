378e00433342309553b9cc5287c4b369727b66f1
nick henderson <nickh1225@gmail.com>
2025-03-24 15:04:32 -0700

    send error email from lambda

--- quickstart.py
+++ quickstart.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta, timezone
 
+import boto3
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-from auth import authenticate_gmail
-
+from auth import authenticate_gmail, is_running_in_lambda
 
 def main():
   from email_utils import get_email_contents
@@ -52,10 +52,32 @@ def main():
         get_email_contents(service, msg["id"])
     print("-------------")
 
-  except HttpError as error:
-    # TODO(developer) - Handle errors from gmail API.
-    print(f"An error occurred: {error}")
+  except Exception as error:
+    if is_running_in_lambda():    
+        send_email(error)
+    else:
+      print(f"An error occurred: {error}")
 
+def send_email(error):
+  ses_client = boto3.client("ses", region_name="us-west-1")
+
+  sender_email = "nickh1225@gmail.com"
+  recipient_email = "nickh1225@gmail.com"
+  subject = "AWS Lambda Failure"
+  body_text = f"Failure when running lamdba function error: {error}"
+
+  try:
+      response = ses_client.send_email(
+          Source=sender_email,
+          Destination={"ToAddresses": [recipient_email]},
+          Message={
+              "Subject": {"Data": subject},
+              "Body": {"Text": {"Data": body_text}},
+          },
+      )
+      print("Email sent successfully! Message ID:", response["MessageId"])
+  except Exception as e:
+      print("Error sending email:", str(e))
 
 if __name__ == "__main__":
   main()
